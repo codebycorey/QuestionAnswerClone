f2f56e032d9da5b584b67464cd655a714c0b5d79
rodonnel <redonnel@cs.odu.edu>
2015-03-12 00:28:48 -0400

    testing database

--- ajaxvote.php
+++ ajaxvote.php
@@ -3,39 +3,43 @@
  * jQuery Voting System
  * @link http://www.w3bees.com/2013/09/voting-system-with-jquery-php-and-mysql.html
  */
-require_once 'includes/constants.php';
+
+include('config.php');
 # start new session
-#
+session_start();
+
 if ($_SERVER['HTTP_X_REQUESTED_WITH']) {
 	if (isset($_POST['postid']) AND isset($_POST['action'])) {
 		$postId = (int) mysql_real_escape_string($_POST['postid']);
 		# check if already voted, if found voted then return
+		if (isset($_SESSION['vote'][$postId])) return;
 		# connect mysql db
-    $link = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME)  or
-      die('There was a problem connecting to the database');
-		# query into db table to know current voting score
-		$query = "SELECT score
-			from answer
-			WHERE id = $postId
-			LIMIT 1";
+		dbConnect();
+
+		# query into db table to know current voting score 
+		$query = mysql_query("
+			SELECT vote
+			from voting
+			WHERE id = '{$postId}' 
+			LIMIT 1" );
 
 		# increase or dicrease voting score
-		if (mysqli_query($link, $query)) {
+		if ($data = mysql_fetch_array($query)) {
 			if ($_POST['action'] === 'up'){
-				$score = ++$data['score'];
+				$vote = ++$data['vote'];
 			} else {
-				$score = --$data['score'];
+				$vote = --$data['vote'];
 			}
 			# update new voting score
-			$query2 = "UPDATE answer
-				SET score = $score
-				WHERE id = $postId";
-
-			mysqli_query($link, $query2);
+			mysql_query("
+				UPDATE voting
+				SET vote = '{$vote}'
+				WHERE id = '{$postId}' ");
 
 			# set session with post id as true
+			$_SESSION['vote'][$postId] = true;
 			# close db connection
-			return true;
+			dbConnect(false);
 		}
 	}
 }
--- index.php
+++ index.php
@@ -1,50 +1,51 @@
 <?php
+/**
+ * @link http://www.w3bees.com/2013/09/voting-system-with-jquery-php-and-mysql.html
+ */
+include('config.php');
+# connect mysql db
+dbConnect();
 
-require_once 'classes/User.php';
-$user = New User();
-$user->confirm_User();
-
-$link = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME)  or
-  die('There was a problem connecting to the database');
-
-$query = "SELECT id, title
-          FROM question";
-
-$result = $link->query($query)
-  or die($link->error);
-
-$num_questions = $result->num_rows;
-
-$question_Table = '';
-  while($row = $result->fetch_assoc()) {
-    $question_id = $row['id'];
-    $question_title = $row['title'];
-
-  $question_Table .=<<<EOD
-  <tr>
-    <td><a href="displayQuestion.php?question_id=$question_id">$question_title</a></td>
-  </tr>
-EOD;
-    }
-
-    $result->free();
-    $link->close();
+$query = mysql_query(
+	'SELECT id, first_name, last_name, film_info, vote 
+	FROM  voting
+	LIMIT 0 , 15');
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>jQUery Voting System</title>
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+	<div class="wrap">
+		<h1><a href="http://www.w3bees.com/2013/09/voting-system-with-jquery-php-and-mysql.html">jQuery Ajax Voting System</a></h1>
 
-<!DOCTYPE html>
-<html>
-  <head>
-  </head>
+		<?php while($row = mysql_fetch_array($query)): ?>
+		<div class="item" data-postid="<?php echo $row['id'] ?>" data-score="<?php echo $row['vote'] ?>">
+			<div class="vote-span"><!-- voting-->
+				<div class="vote" data-action="up" title="Vote up">
+					<i class="icon-chevron-up"></i>
+				</div><!--vote up-->
+				<div class="vote-score"><?php echo $row['vote'] ?></div>
+				<div class="vote" data-action="down" title="Vote down">
+					<i class="icon-chevron-down"></i>
+				</div><!--vote down-->
+			</div>
 
-  <body>
-    <p> Welcome "<?php echo $_SESSION['user_id'];?>!"
-    <a href="login.php?status=loggedout">Log Out</a> </p>
-    <p>
-      <a href="askQuestion.php">Ask a question</a>
-    </p>
-    <h2>List of Questions</h2>
-    <table>
-      <?php echo $question_Table?>
-    </table>
-  </body>
+			<div class="post"><!-- post data -->
+				<h2><?php echo $row['first_name'].' '.$row['last_name']?></h2>
+				<p><?php echo $row['film_info'] ?></p>
+			</div>
+		</div><!--item-->
+		<?php endwhile?>
+
+		<p style="text-align:center; margin-top: 20px">&copy w3bees.com 2013</p>
+	</div>
+	<?php dbConnect(false); ?>
+</body>
 </html>
